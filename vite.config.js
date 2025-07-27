5ada92648904d50b1d1b9f98bf190abf14abd4bc
Robert Au <19robertau132@gmail.com>
2025-07-26 18:08:05 -0700

    rename

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
-  base: "/polygon-box-tessellation",
+  base: "/polygon-box-tessellation/",
   build: {
     outDir: "dist",
     assetsDir: "assets",
